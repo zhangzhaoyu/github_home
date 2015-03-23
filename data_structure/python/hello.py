12722a6afd44b02571b0d680f6d641589b30bc70
zhangzhaoyu <zhangzhaoyu0524@163.com>
2015-03-24 00:09:00 +0800

    init github_home

---
+++ data_structure/python/hello.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+
+def sumN(n) :
+    sum = 0
+    for i in range(1, n + 1) :
+        sum += i
+    return sum
+
+def sumWiseN(n) :
+    sum = ((n + 1) * n) / 2
+    return sum
+
+if __name__ == '__main__'  :
+    print sumWiseN(100)
